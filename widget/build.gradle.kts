5a695cd277d5aabdf809a6fc80a26d44b006f7ae
PeterXi <peter12757@126.com>
2024-07-08 18:05:06 +0800

    new

--- widget/build.gradle.kts
+++ widget/build.gradle.kts
@@ -12,9 +12,6 @@ android {
 
     defaultConfig {
         minSdk = 24
-        aarMetadata {
-            minCompileSdk = 24
-        }
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
